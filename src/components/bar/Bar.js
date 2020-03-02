54bf9411b074363441c5780df9d2b166b1d0127c
Alban <a.veux@laposte.net>
2020-03-02 23:39:12 +0100

    navbar v1

--- src/components/bar/Bar.js
+++ src/components/bar/Bar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./Bar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Bar = () => {
   return (
@@ -9,22 +11,25 @@ const Bar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <Link className="nav-item" to="/">
-            Home
+          <Link className="text-decoration-none ml-5" to="/">
+            HOME
           </Link>
-          <Link className="nav-item" to="/cv">
+          <Link className="text-decoration-none ml-5" to="/cv">
             My CV
           </Link>
-
-          <NavDropdown
-            className="nav-item"
-            title="Project"
-            id="basic-nav-dropdown"
-          >
-            <Link to="/nasa">N.A.S.A</Link>
-          </NavDropdown>
+          <Link className="text-decoration-none ml-5" to="/nasa">
+            N.A.S.A
+          </Link>
         </Nav>
       </Navbar.Collapse>
+      <Navbar.Text className="mr-5">
+        <a className="mr-2" href="https://github.com/albanSimplon">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+        <a href="https://www.linkedin.com/in/alban-veux/">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+      </Navbar.Text>
     </Navbar>
   );
 };
