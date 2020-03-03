3ac4d4450e316fc8a5831e57f2d4691b67a540bb
Alban <a.veux@laposte.net>
2020-03-03 18:41:14 +0100

    background mainpage v2 and navbar v2

--- src/components/bar/Bar.js
+++ src/components/bar/Bar.js
@@ -7,9 +7,12 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Bar = () => {
   return (
-    <Navbar expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar expand="lg" bg="black">
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        className="btn btn-light active"
+      />
+      <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav>
           <Link className="text-decoration-none ml-5" to="/">
             HOME
@@ -23,11 +26,11 @@ const Bar = () => {
         </Nav>
       </Navbar.Collapse>
       <Navbar.Text className="mr-5">
-        <a className="mr-2" href="https://github.com/albanSimplon">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
+        <a className="mr-2 ml-2" href="https://github.com/albanSimplon">
+          <FontAwesomeIcon icon={faGithub} size="2x" inverse />
         </a>
         <a href="https://www.linkedin.com/in/alban-veux/">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          <FontAwesomeIcon icon={faLinkedin} size="2x" inverse />
         </a>
       </Navbar.Text>
     </Navbar>
